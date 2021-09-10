0138e485a8b5d5fde3cffdfa3c31eaca6a324f1c
Mingfeng Wang <mingfeng@MingfengdeMacBook-Pro.local>
2021-09-10 17:42:57 +0800

    :construction: 文档编写

--- main.5m.sh
+++ main.5m.sh
@@ -3,18 +3,22 @@
 #  <xbar.title>mac日历提醒</xbar.title>
 #  <xbar.version>v1.0</xbar.version>
 #  <xbar.author>蒙德伊彼</xbar.author>
-#  <xbar.author.github></xbar.author.github>
+#  <xbar.author.github>git@github.com:cangyan/xbar-mac-calendar.git</xbar.author.github>
 #  <xbar.desc>3天内日程提醒</xbar.desc>
 #  <xbar.image></xbar.image>
 #  <xbar.dependencies></xbar.dependencies>
 
 export PATH="/usr/local/bin:$PATH"
 
-# 常量定义 今日 & 未来2天
-today=`date "+%Y%m%d"`
+
+
+
 
 todayNoEventMessage=":tada: 今日无日程安排!!!"
 
+
+# 常量定义 今日 & 未来2天
+today=`date "+%Y%m%d"`
 
 # 查找符合日期的行程
 icsList=$(find ~/Library/Calendars -name "*.ics")
