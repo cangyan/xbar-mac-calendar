b3c8c1157ecc626dbc3d593dbc5001ada349ba97
Mingfeng Wang <me.wmf@foxmail.com>
2021-09-10 16:53:06 +0800

    :tada: init

---
+++ main.5m.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+#  <xbar.title>mac日历提醒</xbar.title>
+#  <xbar.version>v1.0</xbar.version>
+#  <xbar.author>蒙德伊彼</xbar.author>
+#  <xbar.author.github></xbar.author.github>
+#  <xbar.desc>3天内日程提醒</xbar.desc>
+#  <xbar.image></xbar.image>
+#  <xbar.dependencies></xbar.dependencies>
+
+export PATH="/usr/local/bin:$PATH"
+
+# 常量定义 今日 & 未来2天
+today=`date "+%Y%m%d"`
+
+todayNoEventMessage=":tada: 今日无日程安排!!!"
+
+
+# 查找符合日期的行程
+icsList=$(find ~/Library/Calendars -name "*.ics")
+
+showIcsList=()
+todayList=()
+
+echo "---"
+for icsFile in ${icsList}
+do
+    file=$(cat $icsFile | awk -v FS=':' -v today="${today}" -v file="${icsFile}" '{if (substr($1,0,7)=="DTSTART"&&substr($2,0,8)==today)   print file}')
+    if [ ! -z "$file" -a "$file" != " " ]; then
+      #   cat $file
+      echo "$(cat $file| grep -e "DESCRIPTION") | color=purple"
+    fi
+done
+
+# 抽取数据
