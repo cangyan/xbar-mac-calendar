397de38798a8447757976540e6435a8dfc1c7b7a
Mingfeng Wang <mingfeng@MingfengdeMacBook-Pro.local>
2021-09-14 16:00:53 +0800

    :construction: 格式化条目

--- main.1m.py
+++ main.1m.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/bin/python3
+#!/usr/bin/env PYTHONIOENCODING=UTF-8 TIME_ZONE='Asia/Shanghai' USE_TZ=True /usr/bin/python3
 #
 #  <xbar.title>mac日历提醒</xbar.title>
 #  <xbar.version>v1.0</xbar.version>
@@ -10,7 +10,7 @@
 
 import os
 import glob
-import json
+from datetime import datetime
 
 
 class Cal:
@@ -21,6 +21,8 @@ class Cal:
         self.loc = loc
         self.desc = desc
 
+    def to_display(self):
+        print(self.summary, self.start, self.end)
 
 
 userHome = os.path.expanduser('~')
@@ -29,7 +31,8 @@ path = userHome+"/Library/Calendars"
 
 icsList = glob.glob(path + "/**/*.ics", recursive=True)
 
-i = 0
+days = 10
+calList = []
 
 for fileName in icsList:
     cal = {}
@@ -38,6 +41,7 @@ for fileName in icsList:
     keyStack = []
     with open(fileName, "r") as f:
         for line in f:
+            cal["file"] = fileName
             l = str.split(line, ":")
             # print(l)
             if (l[0] == "BEGIN" and (l[1] == "VCALENDAR\n" or l[1] == "VEVENT\n" or l[1] == "VALARM\n")):
@@ -53,11 +57,49 @@ for fileName in icsList:
                     key = keyStack[len(keyStack)-1]
                 continue
             # print(key, keyStack)
-            if len(l)>1:
-                cal[key].update({l[0].strip():l[1].strip().rstrip("\n")})
+            if len(l) > 1:
+                cal[key].update({l[0].strip(): l[1].strip().rstrip("\n")})
+    if len(cal.keys()) > 0:
+        calList.append(cal)
 
-    i += 1
-    if (i > 2):
-        break
+for item in calList:
+    if len(item["VEVENT"].keys()) > 0:
+        start = ""
+        # 过滤条目
+        if "TRANSP" in item["VEVENT"]:
+            if item["VEVENT"]["TRANSP"] != "OPAQUE":
+                continue
+        # 20190809T160000
+        if "DTSTART;TZID=Asia/Shanghai" in item["VEVENT"]:
+            start = datetime.strptime(
+                item["VEVENT"]["DTSTART;TZID=Asia/Shanghai"], "%Y%m%dT%H%M%S")
+        if "DTSTART;VALUE=DATE" in item["VEVENT"]:
+            start = datetime.strptime(
+                item["VEVENT"]["DTSTART;VALUE=DATE"], "%Y%m%d")
 
-    print(json.dumps(cal))
+        if "DTEND;TZID=Asia/Shanghai" in item["VEVENT"]:
+            end = datetime.strptime(
+                item["VEVENT"]["DTEND;TZID=Asia/Shanghai"], "%Y%m%dT%H%M%S")
+        if "DTEND;VALUE=DATE" in item["VEVENT"]:
+            end = datetime.strptime(
+                item["VEVENT"]["DTEND;VALUE=DATE"], "%Y%m%d")
+
+        summary = ""
+        if "SUMMARY" in item["VEVENT"]:
+            summary = item["VEVENT"]["SUMMARY"]
+
+        loc = ""
+        if "LOCATION" in item["VEVENT"]:
+            loc = item["VEVENT"]["LOCATION"]
+
+        desc = ""
+        if "DESCRIPTION" in item["VEVENT"]:
+            desc = item["VEVENT"]["DESCRIPTION"]
+
+        today = datetime.today()
+
+        diff = start - today
+        if diff.days >= 0 and diff.days < days:
+            # print(item["file"])
+            c = Cal(start, end, summary, loc, desc)
+            c.to_display()
